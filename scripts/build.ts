c2113458a4844a15a27785800d0995c9b865ee42
Valerij <hi@valerij.me>
2025-08-13 08:40:13 +0200

    Fix output path for webmanifest file in build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,3 @@
 import manifest from "#src/manifest.json" with { type: "json" };
 import { writeFileSync } from "node:fs";
-writeFileSync("docs/playground/manifest.webmanifest", JSON.stringify(manifest));
+writeFileSync("docs/manifest.webmanifest", JSON.stringify(manifest));
